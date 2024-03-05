88f09a8e64ac2a945d283a43dacfde21e59f8629
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-05 00:56:47 -0800

    added mobile responsive menu button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,32 @@
 // Title should be highlighted to indicate current section user is on
 // Clicking links should change the browser URL and display the corresponding section
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import styles from '../styles/Navbar.module.css';
 
 export const Navbar = () => {
-    return <nav className={styles.navbar}>
-        <a className={styles.title} href="/">
-            Portfolio
-        </a>
-        <div className={styles.menu} >
-        <ul className={styles.menuItems}>
-            <li><a href="#about">About Me</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#resume">Resume</a></li>
-        </ul>
-        </div>
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-    </nav>
+    return (
+        <nav className={styles.navbar}>
+            <a className={styles.title} href="/">
+                Portfolio
+            </a>
+            <div className={styles.menu} >
+                <button className={styles.menuBtn} onClick={toggleMenu}>
+                    {menuOpen ? 'Close' : 'Menu'}
+                </button>
+                <ul className={`${styles.menuItems} ${menuOpen ? styles.menuOpen : ''}`}
+                    onClick={() => setMenuOpen(false)}>
+                    <li><a href="#about">About Me</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#resume">Resume</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
